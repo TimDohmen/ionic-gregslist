ebfac710ef086c8ffef4ebb81d5f71f72f417ba0
Tim Dohmen <dohmentim@yahoo.com>
2021-05-04 10:21:31 -0700

    historymode on server

--- server/server/main.js
+++ server/server/main.js
@@ -3,9 +3,11 @@ import Socket from './services/SocketService'
 import Startup from './Startup'
 import DbContext from './db/DbConfig'
 import { logger } from './utils/Logger'
+import history from 'connect-history-api-fallback'
 
 // create server & socketServer
 const app = express()
+app.use(history())
 const socketServer = require('http').createServer(app)
 const io = require('socket.io')(socketServer)
 const port = process.env.PORT || 3000
